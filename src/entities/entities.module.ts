885cfa7a0554856230175c72f27178e538f42ac3
cnunezjacas <cnunezjacas@gmail.com>
2026-07-29 11:23:33 -0400

    Importado y cargado correctamente el módulo "TerminalModule"

--- src/entities/entities.module.ts
+++ src/entities/entities.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PuntoDeVentaModule } from './punto_de_venta/punto-de-venta.module';
 import { TerminalModule } from './terminal/terminal.module';
-
 import { EstacionesModule } from './estaciones/estaciones.module';
-
 import { CuentaModule } from './estaciones/cuenta.module';
-
 import { AreaDeTrabajoModule } from './areas_de_trabajo/area-de-trabajo.module';
 
 @Module({
