c7794fc8012adf83848f5b48414dbd1ff9266699
cnunezjacas <cnunezjacas@gmail.com>
2026-07-31 13:37:28 -0400

    Importados, cargados y probados correctamente los módulos TramitadorModule, ClienteModule y OperadorModule.

--- src/entities/entities.module.ts
+++ src/entities/entities.module.ts
@@ -4,9 +4,14 @@ import { TerminalModule } from './terminal/terminal.module';
 import { EstacionesModule } from './estaciones/estaciones.module';
 import { CuentaModule } from './estaciones/cuenta.module';
 import { AreaDeTrabajoModule } from './areas_de_trabajo/area-de-trabajo.module';
+import { OperadorModule } from './operador/operador.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { TramitadorModule } from './tramitador/tramitador.module';
 
 @Module({
-  imports: [
+  imports: [ TramitadorModule,
+    ClienteModule,
+    OperadorModule,
     TerminalModule,
     PuntoDeVentaModule,
     EstacionesModule,
